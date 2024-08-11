ab2a4604b66263facdf04e9ded02428595ad8f0b
md-redwanul-haque <redwan229900@gmail.com>
2024-08-11 12:09:27 +0600

    Initial Commit

---
+++ bootcamp/bootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  bootcampApp.swift
+//  bootcamp
+//
+//  Created by Redwan on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct bootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
