038a12270963be2a6d32a5569cfeb451c11104e3
md-redwanul-haque <redwan229900@gmail.com>
2024-08-13 15:28:35 +0600

    padding lesson done

--- bootcamp/PaddingTutorial.swift
+++ bootcamp/PaddingTutorial.swift
@@ -9,10 +9,24 @@ import SwiftUI
 
 struct PaddingTutorial: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .background(Color.blue)
-            .padding([.horizontal,.bottom], 35)
-            .background(Color.red)
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//            .background(Color.blue)
+//            .padding([.horizontal,.top], 35)
+//            .background(Color.red)
+        
+        VStack(alignment:.leading,spacing: 24){
+            
+            Text("Your App genres")
+                .font(.title3)
+                .fontWeight(.semibold)
+                .padding(.leading, 4)
+            
+            Text("Lorem Ipsumdfsfdsfasdfalsdfljsdjfklsadkljslakdjlasd sdjfsdkflk sjdlfjslkdjf9sdfh fisofiiofnsdlf fjdfnfsdif sifjnsdlknfi jdsfisdnif sjsdafskd sdjfsaf")
+                .padding(.leading,12)
+            
+            
+        }
+        
     }
 }
 
