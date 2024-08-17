d43d1c749c240fd13c01e84786cbc4f7e3e2b51f
md-redwanul-haque <redwan229900@gmail.com>
2024-08-17 12:48:06 +0600

    image added

--- bootcamp/ScrollViewTutorial.swift
+++ bootcamp/ScrollViewTutorial.swift
@@ -8,17 +8,53 @@
 import SwiftUI
 
 struct ScrollViewTutorial: View {
+    var imageList = ["im1",
+                  "im2",
+                  "im3",
+                  "im4",
+                  "im5",]
+    
+    
     var body: some View {
-        ScrollView(.horizontal){
-            HStack{
-                ForEach( 0..<50 , id: \.self) { index in
-                    Rectangle()
-                        .frame(width: 50,height: 50)
-                        .foregroundColor(.red)
-                    
-                    
+        VStack(alignment:.leading){
+            Text("Tampa Bay")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .padding()
+            ScrollView(.vertical, showsIndicators: false){
+                VStack(spacing: 24){
+                    ForEach(imageList, id: \.self ){ image in
+                            Image(image)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(height: 148)
+                            .clipped()
+                            .cornerRadius(15)
+                            
+                        HStack{
+                            VStack(alignment: .leading){
+                                Text("Haiku - Tempa")
+                                    .font(.system(size: 14))
+                                    .fontWeight(.semibold)
+                                Text("$0.99 deliver Fee | 30-40 min")
+                                    .font(.caption)
+                                    .foregroundColor(.gray)
+                                
+                            }
+                            Spacer()
+                            
+                            Text("4.4")
+                                .font(.caption2)
+                                .padding(6)
+                                .background(Color(.systemGray5))
+                                .clipShape(Circle())
+                        }
+                    }
+                    .padding(.horizontal)
+                  
                 }
             }
+            
             
             
         }
