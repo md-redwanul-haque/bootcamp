91173aac444ddf6d193452720cd9164d7620fc4f
md-redwanul-haque <redwan229900@gmail.com>
2024-08-20 17:36:18 +0600

    state property done

---
+++ bootcamp/ControlFlowTutorial.swift
@@ -0,0 +1,18 @@
+//
+//  ControlFlowTutorial.swift
+//  bootcamp
+//
+//  Created by Redwan on 19/8/24.
+//
+
+import SwiftUI
+
+struct ControlFlowTutorial: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ControlFlowTutorial()
+}
