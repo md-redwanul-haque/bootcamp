eb88d5636f09f61b722ec34ceb20161a71a55785
md-redwanul-haque <redwan229900@gmail.com>
2024-08-21 11:18:47 +0600

    loop tutorial code refact , code break , data pass , array data pass

---
+++ loopTutorialCodeBreak.swift
@@ -0,0 +1,18 @@
+//
+//  loopTutorialCodeBreak.swift
+//  bootcamp
+//
+//  Created by Redwan on 21/8/24.
+//
+
+import SwiftUI
+
+struct loopTutorialCodeBreak: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    loopTutorialCodeBreak()
+}
