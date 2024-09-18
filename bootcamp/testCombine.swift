c17744bee1b9660d26cedb4141c301ee1b522362
md-redwanul-haque <redwan229900@gmail.com>
2024-09-18 16:51:22 +0600

    combine lesson done

---
+++ bootcamp/testCombine.swift
@@ -0,0 +1,18 @@
+//
+//  testCombine.swift
+//  bootcamp
+//
+//  Created by Redwan on 18/9/24.
+//
+
+import SwiftUI
+
+struct testCombine: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    testCombine()
+}
